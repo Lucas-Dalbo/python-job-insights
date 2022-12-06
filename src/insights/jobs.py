52014ab8c94fc2c18fb59d0ad077f515bd6ea77c
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-12-06 15:49:38 -0300

    feat: Implementa função read - função lê .csv e retorna uma lista de dict com os dados

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
@@ -16,7 +17,11 @@ def read(path: str) -> List[Dict]:
     list
         List of rows as dicts
     """
-    raise NotImplementedError
+    with open(path, encoding="utf-8") as file:
+        open_file = csv.DictReader(file, delimiter=",", quotechar='"')
+        jobs_list = [row for row in open_file]
+
+    return jobs_list
 
 
 def get_unique_job_types(path: str) -> List[str]:
