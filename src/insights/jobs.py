cd98b87bd941df891f188bb4dfe5e8479344f51c
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-12-06 16:19:22 -0300

    feat: Implementa Função filter_by_job_type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -60,4 +60,6 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     list
         List of jobs with provided job_type
     """
-    raise NotImplementedError
+    filtered_jobs = [job for job in jobs if job["job_type"] == job_type]
+
+    return filtered_jobs
