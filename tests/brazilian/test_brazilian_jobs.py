3ba989edad386a5ec61903c1408d3ea715f1edb0
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-12-07 08:47:40 -0300

    test: Tenta função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,15 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    expected_keys = {"title", "salary", "type"}
+
+    file = "tests/mocks/brazilians_jobs.csv"
+    result = read_brazilian_file(file)
+
+    keys = set()
+    for job in result:
+        for key in job.keys():
+            keys.add(key)
+
+    assert keys == expected_keys
